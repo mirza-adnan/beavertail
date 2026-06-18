34cb9c9d9812fb515f583eb338e552c317e993bd
Mirza Adnan <mirza.adnan2205@gmail.com>
2026-06-18 21:22:57 +0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -9,5 +9,9 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
+	var cmd string
 	fmt.Print("$ ")
+	fmt.Scanln(&cmd)
+
+	fmt.Printf("%v: %v command not found", cmd, cmd)
 }
